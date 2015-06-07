77d8a85ddd99bf79bb29189553d4b9102c19eae3
Kenley Arai <me@kenleyarai.com>
2015-06-07 13:13:26 -0700

    Added main loop

--- python/main.py
+++ python/main.py
@@ -1,7 +1,16 @@
 import psycopg2
 
+def option_handler(option):
+    if option == "1":
+        return True
+
 def main():
-    return
+    exit = False
+    while exit != True:
+        print("1. Exit")
+        option = input("Please choose an option: ")
+
+        exit = option_handler(option)  
 
 if __name__ == "__main__":
     main()
