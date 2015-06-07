bfff368637d82657734a91856f1b3f69b894271d
Kenley Arai <me@kenleyarai.com>
2015-06-07 13:50:12 -0700

    Added postgresql connection and login command

--- python/main.py
+++ python/main.py
@@ -1,22 +1,31 @@
 import psycopg2
-
 def option_handler(option):
     if option == '1':
         return True
 
-def login_handler(option):
+def login(username, password, db_conn):
+    cur = db_conn.cursor()
+    cur.execute("SELECT USER_ID, PASSWORD FROM USR WHERE password=%s AND USER_ID=%s" % username, password)
+    rows = cur.fetchall()
+    print(rows[0])
+    return True
+    
+
+def login_handler(option, db_conn):
     if option == "1":
-        print('login')
+        login    = input('Username: ')
+        password = input('Password: ')
+        return login(username, password, db_conn)
     elif option == "2":
         print('register')
     return False
 
-def prompt_handler(logged_in):
+def prompt_handler(logged_in, db_conn):
     if not logged_in:
         print('1. Login')
         print('2. Register')
         option = input('Please choose an option: ')
-        return login_handler(option)
+        return login_handler(option, db_conn)
     elif logged_in: 
         print('1. Exit')
         option = input('Please choose an option: ')
@@ -24,11 +33,22 @@ def prompt_handler(logged_in):
 
 
 def main():
+    # Initialize Db
+    try:
+    db_conn = psycopg2.connect(
+                               host     = ""
+                               user     = ""
+                               database = ""
+                               password = ""
+                               )
+    except psycopg2.DatabaseError as e:
+        raise e
+            sys.exit(1)
     exit      = False
     logged_in = False
 
     while not exit:
-        exit = prompt_handler(logged_in)
+        exit = prompt_handler(logged_in, db_conn)
 
 if __name__ == "__main__":
     main()
