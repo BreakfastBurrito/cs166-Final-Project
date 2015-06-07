1da6a4c006389ccd2169348620f7e3e3a0a57c4d
Kenley Arai <me@kenleyarai.com>
2015-06-07 13:27:15 -0700

    Added login manager

--- python/main.py
+++ python/main.py
@@ -1,16 +1,34 @@
 import psycopg2
 
 def option_handler(option):
-    if option == "1":
+    if option == '1':
         return True
 
-def main():
-    exit = False
-    while exit != True:
-        print("1. Exit")
-        option = input("Please choose an option: ")
+def login_handler(option):
+    if option == "1":
+        print('login')
+    elif option == "2":
+        print('register')
+    return False
 
-        exit = option_handler(option)  
+def prompt_handler(logged_in):
+    if not logged_in:
+        print('1. Login')
+        print('2. Register')
+        option = input('Please choose an option: ')
+        return login_handler(option)
+    elif logged_in: 
+        print('1. Exit')
+        option = input('Please choose an option: ')
+        return option_handler(option)
+
+
+def main():
+    exit      = False
+    logged_in = False
+
+    while not exit:
+        exit = prompt_handler(logged_in)
 
 if __name__ == "__main__":
     main()
