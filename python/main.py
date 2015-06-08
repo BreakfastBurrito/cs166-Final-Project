08d391e345ccd006a473221d86689625ef3b87e7
Kenley Arai <me@kenleyarai.com>
2015-06-07 20:40:29 -0700

    Finished reading messages

--- python/main.py
+++ python/main.py
@@ -1,25 +1,6 @@
-from datetime import datetime
 import psycopg2
-
-def pass_check(pass1,pass2):
-    return pass1 == pass2
-
-def name_space_check(name):
-    return " " in name
-
-def date_format_check(d):
-    try:
-        return datetime.strptime(d, '%Y/%m/%d')
-    except ValueError:
-        print("Incorrect dateformat: ")
-        return False
-
-
-def valid_option(option, options):
-    if option in options:
-        return True
-    print('\nInvalid option\n')
-    return False
+from user import *
+from helper import *
 
 def search(db_conn):
     search_name = input('\nSearch: ')
@@ -29,17 +10,77 @@ def search(db_conn):
     try:
         cur.execute("SELECT NAME FROM USR WHERE NAME LIKE '%s%%' LIMIT 10" % search_name)
     except psycopg2.DatabaseError as e:
-        return True, False
+        return True
     
     for row in cur.fetchall():
         print(row[0])
     print("\n")
-    return True, False
+    return True
     
 
-def option_handler(option, db_conn):
+def get_sent_messages(db_conn, uname):
+    cur = db_conn.cursor()
+    try:
+        cur.execute("SELECT * FROM MESSAGE WHERE senderid='%s' AND status != 'Draft'" % uname.username)
+    except psycopg2.DatabaseError as e:
+        print(e)
+        return True
+
+    for row in cur.fetchall():
+        for item in row:
+            print(item)
+        print('\n')
+    return True
+
+def get_received_messages(db_conn, uname):
+    cur = db_conn.cursor()
+    try:
+        cur.execute("SELECT * FROM MESSAGE WHERE receiverid='%s' AND status !='Draft' OR status != 'Failed to Deliver'" % uname.username)
+    except psycopg2.DatabaseError as e:
+        print(e)
+        return True
+
+    for row in cur.fetchall():
+        for item in row:
+            print(item)
+        print("\n")
+    return True
+
+def get_drafts(db_conn, uname):
+    cur = db_conn.cursor()
+    try:
+        cur.execute("SELECT * FROM MESSAGE WHERE senderid='%s' AND status = 'Draft'" % uname.username)
+    except psycopg2.DatabaseError as e:
+        print(e)
+        return True
+
+    for row in cur.fetchall():
+        for item in row:
+            print(item)
+        print("\n")
+    return True
+
+def option_handler(option, db_conn, uname):
     # Change password
-    if option == '3':
+
+    if option == '1':
+        print('1. View Sent')
+        print('2. View Received')
+        print('3. View drafts')
+        print('4. Send Message')
+        mesg_type = input('Select option: ')
+        print('\n')
+        while not valid_option(mesg_type, "12345"):
+            mesg_type = input('Select option: ')
+            print('\n')
+        if mesg_type == '1':
+            return get_sent_messages(db_conn, uname)
+        elif mesg_type == '2':
+            return get_received_messages(db_conn, uname)
+        elif mesg_type == '3':
+            return get_drafts(db_conn, uname)
+
+    elif option == '3':
         return search(db_conn) 
     elif option == '4':
         print('\nPlease reauthenticate')
@@ -89,13 +130,14 @@ def register(username, password, name, dob, db_conn):
     except psycopg2.DatabaseError as e:
         print('Error user already exists')
         return False
-    return True
+    return False
 
-def login_handler(option, db_conn):
+def login_handler(option, db_conn, uname):
     if option == "1":
         username = input('Username: ')
         password = input('Password: ')
         if login(username, password, db_conn):
+            uname.username = username
             return True
         else:
             print('\nIncorrect username or password\n')
@@ -121,7 +163,6 @@ def login_handler(option, db_conn):
         while not date_format_check(dob):
             dob = input('Date of birth (yyyy/mm/dd): ')
         return register(username, password, name, dob, db_conn) 
-
     return False
 
 
@@ -139,6 +180,7 @@ def main():
         sys.exit(1)
     exit      = False
     logged_in = False
+    username = User()
 
     while not exit:
         if not logged_in:
@@ -150,9 +192,10 @@ def main():
             if option == "3":
                 exit = True
             elif valid_option(option, "12"):
-                logged_in = login_handler(option, db_conn)
+                logged_in = login_handler(option, db_conn, username)
 
         elif logged_in:
+            print('1. Show Messages')
             print('3. Search for people')
             print('4. Change password')
             print('8. Logout')
@@ -161,8 +204,8 @@ def main():
 
             if option == '9':
                 exit = True
-            elif valid_option(option, "348"):
-                logged_in = option_handler(option, db_conn)
+            elif valid_option(option, "1348"):
+                logged_in = option_handler(option, db_conn, username)
 
 if __name__ == "__main__":
     main()
