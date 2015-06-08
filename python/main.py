d908a6cab498767a3b80c48c97a4a225f075559e
Kenley Arai <me@kenleyarai.com>
2015-06-07 17:46:58 -0700

    Added register functionality

--- python/main.py
+++ python/main.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import psycopg2
+
 
 def valid_option(option, options):
     if option in options:
@@ -21,6 +23,28 @@ def login(username, password, db_conn):
     row = cur.fetchone()
     return row == (username, password) 
 
+def pass_check(pass1,pass2):
+    return pass1 == pass2
+
+def name_space_check(name):
+    return " " in name
+
+def date_format_check(d):
+    try:
+        return datetime.strptime(d, '%Y/%m/%d') 
+    except ValueError:
+        print("Incorrect dateformat: ")
+        return False
+
+
+def register(username, password, name, dob, db_conn):
+    cur = db_conn.cursor()
+    try:
+        cur.execute("INSERT INTO USR VALUES ('%s', '%s', '%s', '%s')" % (username, password, name, dob))
+    except psycopg2.DatabaseError as e:
+        print('Error user already exists')
+        return False
+    return True
 
 def login_handler(option, db_conn):
     if option == "1":
@@ -30,8 +54,29 @@ def login_handler(option, db_conn):
             return True, False
         else:
             print('\nIncorrect username or password\n')
+
     elif option == "2":
-        print('')
+        username  = input('Username: ')
+        password  = input('Password: ')
+        password2 = input('Renter Password: ')
+
+        while not pass_check(password, password2):
+            print("Passwords do not match")
+            password = input('Password: ')
+            password2 = input('Renter Password: ')
+        
+        email = input('Email: ')
+        name = input('Full Name: ')
+
+        while not name_space_check(name):
+            print("Please input your full name with spaces")
+            name = input('Full Name')
+        dob = input("Date of birth (yyyy/mm/dd): ")
+
+        while not date_format_check(dob):
+            dob = input('Date of birth (yyyy/mm/dd): ')
+        return register(username, password, name, dob, db_conn), False
+
     elif option == "3":
         return False, True
     return False, False
@@ -44,7 +89,7 @@ def main():
                                host     = 'breakfastburrito.io',
                                user     = 'db',
                                database = 'db',
-                               password = 'dbuser',
+                               password = '',
                                )
     except psycopg2.DatabaseError as e:
         raise e
