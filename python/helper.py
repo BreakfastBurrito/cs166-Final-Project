b463b787819ab14b2934a1cbb6bff03b90fba2b0
Kenley Arai <me@kenleyarai.com>
2015-06-07 21:40:27 -0700

    added

---
+++ python/helper.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+def pass_check(pass1,pass2):
+    return pass1 == pass2
+
+def name_space_check(name):
+    return " " in name
+
+def date_format_check(d):
+    try:
+        return datetime.strptime(d, '%Y/%m/%d')
+    except ValueError:
+        print("Incorrect dateformat: ")
+        return False
+
+def valid_option(option, options):
+    if option in options:
+        return True
+    print('\nInvalid option\n')
+    return False
