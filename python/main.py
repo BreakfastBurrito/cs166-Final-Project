2fdf0b4f8ecc34a7e651d6d31c85373532b6c9d2
Antoine Guerrero <asguerrero3@gmail.com>
2015-06-07 18:13:52 -0700

    merged

--- python/main.py
+++ python/main.py
@@ -3,14 +3,30 @@ import psycopg2
 
 
 def valid_option(option, options):
-    if option in options:
+    if str(option) in options:
         return True
     print('\nInvalid option\n')
     return False
 
-def option_handler(option):
-    if option == '8':
-        return False, False 
+def option_handler(option, db_conn):
+    # Change password
+    if option == '4':
+        print('\nPlease reauthenticate')
+        username = input('Username: ')
+        password = input('Password: ')
+        reauth_success = login(username, password, db_conn)
+        if reauth_success:
+            new_password = input('New Password: ')
+            new_password2 = input('Renter new password: ')
+            while not pass_check(new_password, new_password2):
+                print("Passwords do not match")
+                new_password = input('New Password: ')
+                new_password2 = input('Renter new password: ')
+            return change_password(username, new_password, db_conn), False
+        else:
+            return False, False
+    elif option == '8':
+        return False, False
     elif option == '9':
         return True, True
 
@@ -21,7 +37,20 @@ def login(username, password, db_conn):
     except psycopg2.DatabaseError as e:
         return False
     row = cur.fetchone()
-    return row == (username, password) 
+    return row == (username, password)
+
+def change_password(username, password, db_conn):
+    success = False
+
+    cur = db_conn.cursor()
+    try:
+        cur.execute("UPDATE USR SET password='%s' WHERE USERID='%s'" % (password, username))
+        db_conn.commit()
+    except psycopg2.DatabaseError as e:
+        print("hello?")
+        return False
+
+    return True
 
 def pass_check(pass1,pass2):
     return pass1 == pass2
@@ -31,7 +60,7 @@ def name_space_check(name):
 
 def date_format_check(d):
     try:
-        return datetime.strptime(d, '%Y/%m/%d') 
+        return datetime.strptime(d, '%Y/%m/%d')
     except ValueError:
         print("Incorrect dateformat: ")
         return False
@@ -41,6 +70,7 @@ def register(username, password, name, dob, db_conn):
     cur = db_conn.cursor()
     try:
         cur.execute("INSERT INTO USR VALUES ('%s', '%s', '%s', '%s', '%s')" % (username, password, email, name, dob))
+        db_conn.commit()
     except psycopg2.DatabaseError as e:
         print('Error user already exists')
         return False
@@ -64,7 +94,7 @@ def login_handler(option, db_conn):
             print("Passwords do not match")
             password = input('Password: ')
             password2 = input('Renter Password: ')
-        
+
         email = input('Email: ')
         name = input('Full Name: ')
 
@@ -89,7 +119,7 @@ def main():
                                host     = 'breakfastburrito.io',
                                user     = 'db',
                                database = 'db',
-                               password = '',
+                               password = 'userdbabc',
                                )
     except psycopg2.DatabaseError as e:
         raise e
@@ -106,15 +136,15 @@ def main():
 
             if valid_option(option, "123"):
                 logged_in, exit = login_handler(option, db_conn)
-            
+
         elif logged_in:
+            print('4. Change password')
             print('8. Logout')
             print('9. Exit')
             option = input('Please choose an option: ')
 
-            if valid_option(option, "89"):
-                exit, logged_in = option_handler(option)
-        db_conn.commit()
+            if valid_option(option, "489"):
+                logged_in, exit = option_handler(option, db_conn)
 
 if __name__ == "__main__":
     main()
