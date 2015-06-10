ad6fc525be6557bca4fdc2143d492db85b7ff38e
Antoine Guerrero <asguerrero3@gmail.com>
2015-06-10 08:05:40 -0700

    Fixed script

--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#psql -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
-#psql -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
-#psql -p $PGPORT $DB_NAME < $DIR/../src/load_data.sql
 
-psql -U db -d db -a -f $DIR/../src/create_indexes.sql
 psql -U db -d db -a -f $DIR/../src/create_tables.sql
+psql -U db -d db -a -f $DIR/../src/create_index.sql
 psql -U db -d db -a -f $DIR/../src/load_data.sql
